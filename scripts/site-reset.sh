2f6bb5fcc691e8c9759aa93c6a51ceba238c0a2c
Mads H. Danquah <mads@danquah.dk>
2017-08-18 20:44:07 +0200

    wip

--- scripts/site-reset.sh
+++ scripts/site-reset.sh
@@ -13,14 +13,18 @@ cp -vR ${SCRIPT_DIR}/../docker/sample-assets/* "${SCRIPT_DIR}/../web/sites/defau
 # Reset the site. If the aleph module exists, run composer for it and enable it
 # if it does not exist, enable the connie module.
 time docker-compose run --entrypoint "sh -c" --rm php " \
-  (test -d /var/www/web/profiles/ding2/modules/aleph ; composer --working-dir=/var/www/web/profiles/ding2/modules/aleph install) && \
+  echo '*** Composer installing' && \
   composer --working-dir=/var/www/web/profiles/ding2/modules/ding_test install && \
   composer --working-dir=/var/www/web/profiles/ding2/modules/fbs install && \
+  (test ! -d /var/www/web/profiles/ding2/modules/aleph || composer --working-dir=/var/www/web/profiles/ding2/modules/aleph install) && \
+  echo '*** Disabeling and enabling modules' && \
   drush dis alma -y && \
-  (test -d /var/www/web/profiles/ding2/modules/aleph ; drush en aleph -y) && \
-  (test -d /var/www/web/profiles/ding2/modules/aleph || drush en connie -y) && \
   drush en ding_test -y && \
+  (test ! -d /var/www/web/profiles/ding2/modules/aleph || (echo '*** Aleph detected, enabling and disabling Connie' && drush en aleph -y && drush dis connie -y)) && \
+  (test -d /var/www/web/profiles/ding2/modules/aleph || (echo '*** Asing connie' && drush en connie -y)) && \
+  echo '*** Running updb' && \
   drush updb -y && \
+  echo '*** Setting variables' && \
   drush vset ting_search_url http://oss-services.dbc.dk/opensearch/4.2/ && \
   drush vset ting_search_profile test && \
   drush vset ting_agency 100200 && \
@@ -28,5 +32,6 @@ time docker-compose run --entrypoint "sh -c" --rm php " \
   drush vset aleph_base_url http://snorri.lb.is/X && \
   drush vset aleph_base_url_rest http://snorri.lb.is:1892/rest-dlf && \
   drush vset aleph_enable_logging TRUE && \
+  echo '*** Clearing cache' && \
   drush cc all
   "
