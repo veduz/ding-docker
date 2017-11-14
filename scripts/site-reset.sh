c7f204e3c8a226a86d1306870f9288d25583460d
Mads H. Danquah <mads@danquah.dk>
2017-11-14 13:59:17 +0100

    Add missing opensearch_recommendation_url variable to reset

--- scripts/site-reset.sh
+++ scripts/site-reset.sh
@@ -73,6 +73,7 @@ time docker-compose exec php sh -c "\
     \"minimumString\": \"3\"
 }' | drush variable-set --format=json ting_search_autocomplete_settings - && \
   drush variable-set opensearch_enable_logging 1 && \
+  drush variable-set opensearch_recommendation_url 'http://openadhl.addi.dk/1.1/' && \
   drush variable-set ting_search_profile test && \
   drush variable-set ding_serendipity_isslow_timeout 20 && \
   drush variable-set opensearch_agency 100200 && \
